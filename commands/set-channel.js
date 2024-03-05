7717ac189a93129e6a4ad822e1d9cb9e22591539
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-03-05 07:27:57 +0700

    add guild text require type to channel option

--- commands/set-channel.js
+++ commands/set-channel.js
@@ -1,4 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const fs = require('fs')
+const dataChannel = require('../data/data.json')
+
+// console.log(dataChannel['1212461534861729792'] === undefined)
+// console.log(dataChannel['12124615348617297923'] === undefined)
+// dataChannel['12124615348617297923'] = {
+//     channel: '12345678'
+// }
+// fs.writeFileSync('../data/data.json', JSON.stringify(dataChannel))
+// console.log(dataChannel['12124615348617297923'] === undefined)
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,5 +17,6 @@ module.exports = {
         .addChannelOption(option =>
             option.setName('channel')
                 .setDescription('Kênh chơi nối từ')
+                .addChannelTypes(ChannelType.GuildText)
                 .setRequired(true))
 }
