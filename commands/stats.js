f615a1cda9d5c74f7ace404a0dba6465c93c52c1
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-05-12 02:48:10 +0700

    :tada: init stats command

---
+++ commands/stats.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const fs = require('fs')
+const path = require('path')
+const statsFile = path.resolve(__dirname, '../data/stats.txt')
+const queryPath = path.resolve(__dirname, '../data/query.txt')
+
+let queryNumber = '0'
+
+fs.readFile(queryPath, 'utf-8', (err, data) => {
+    if (!err) {
+        queryNumber = data
+    }
+});
+
+const statEmbed = (client) => new EmbedBuilder()
+    .setColor(13250094)
+    .addFields(
+        {
+            name: 'Tổng số server đang sử dụng',
+            value: `${client.guilds.cache.size} servers`
+        },
+        {
+            name: 'Tổng số phiên chơi',
+            value: '...'
+        },
+        {
+            name: 'Tổng số truy vấn dữ liệu',
+            value: `${queryNumber}`
+        },
+        {
+            name: 'Tổng số từ trong ngân hàng từ',
+            value: '...'
+        },
+    )
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('Xem các thống kê của BOT'),
+
+        async execute(interaction, client) {
+            await interaction.reply({
+                embeds: [statEmbed(client)],
+                flags: [4096]
+            })
+        }
+}
