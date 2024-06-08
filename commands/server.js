eab7e1ef7d5f302e8ec50b663b58b1178df01a0e
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-06-08 12:36:37 +0700

    :fire: update /server with Embed

--- commands/server.js
+++ commands/server.js
@@ -1,7 +1,53 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, Client, InteractionCollector } = require('discord.js')
 const fs = require('fs')
 const path = require('path')
 
+/**
+ * 
+ * @param {InteractionCollector} interaction 
+ * @param {Client} client
+ * @returns 
+ */
+const serverEmbed = async (interaction, client) => {
+    const guild = interaction.member.guild
+    const owner = await guild.fetchOwner()
+    return new EmbedBuilder()
+        .setColor(13250094)
+        //.setTitle(guild.name)
+        .setAuthor({
+            name: guild.name,
+            iconURL: guild.iconURL({ dynamic: true })
+        })
+        .setThumbnail(guild.iconURL({ dynamic: true }))
+        .addFields(
+            {
+                name: 'ID Server',
+                value: interaction.guildId,
+                inline: true,
+            },
+            {
+                name: 'Thành lập',
+                value: `<t:${Math.floor(Date.parse(guild.createdAt) / 1000)}:R>`,
+                inline: true,
+            },
+            {
+                name: 'Owner',
+                value: `<@${guild.ownerId}>`,
+                inline: true,
+            },
+            {
+                name: 'Ngày thêm Bot',
+                value: `<t:${Math.floor(Date.parse(guild.joinedAt) / 1000)}:R>`,
+                inline: true
+            },
+            {
+                name: 'Premium',
+                value: ':white_check_mark: Đã kích hoạt',
+                inline: true
+            }
+        )
+
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,8 +55,9 @@ module.exports = {
         .setDescription('Xem thông tin máy chủ'),
 
         async execute (interaction, client) {
+            console.log(interaction.member.guild)
             await interaction.reply({
-                content: 'Test'
+                embeds: [await serverEmbed(interaction, client)]
             })
         }
 }
