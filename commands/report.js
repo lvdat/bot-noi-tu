271b90baf8b6976ed98492b58686fa89450f4b5d
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-06-09 10:29:26 +0700

    :fire: init send to report channel function

--- commands/report.js
+++ commands/report.js
@@ -1,6 +1,43 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, InteractionCollector, Client, PermissionsBitField } = require('discord.js')
+const fs = require('fs')
+const path = require('path')
 require('dotenv').config()
 const REPORT_CHANNEL = process.env.REPORT_CHANNEL
+const reportWordsPath = path.resolve(__dirname, './data/report-words.json')
+
+/**
+ * 
+ * @param {String} word 
+ * @param {String} reason 
+ * @param {String} author 
+ * @param {String} svname 
+ * @returns {EmbedBuilder}
+ */
+const reportEmbed = (word, reason, author, svname) => {
+    return new EmbedBuilder()
+        .setColor(13250094)
+        .addFields(
+            {
+                name: 'Từ báo cáo',
+                value: `**${word}**`,
+                inline: true
+            },
+            {
+                name: 'Lý do',
+                value: reason,
+                inline: true
+            },
+            {
+                name: 'Người gửi',
+                value: author
+            },
+            {
+                name: 'Máy chủ',
+                value: svname,
+                inline: true
+            }
+        )
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -31,7 +68,15 @@ module.exports = {
         } else {
             let word = interaction.options.getString('word')
             let reason = interaction.options.getString('reason') ?? 'No reason provided.'
-            
+
+            client.channels.cache.get(REPORT_CHANNEL).send({
+                embeds: [reportEmbed(word, reason, interaction.user.username, interaction.guild.name)]
+            })
+
+            return interaction.reply({
+                content: `Đã báo cáo từ **${word}**`,
+                ephemeral: true
+            })
         }
 
     }
