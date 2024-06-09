987ed6856b35b5e46df922939cef5e78c107ae8f
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-06-09 11:13:17 +0700

    :fire: accept/decline button added to report

--- commands/report.js
+++ commands/report.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, InteractionCollector, Client, PermissionsBitField } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, InteractionCollector, Client, PermissionsBitField, ButtonStyle } = require('discord.js')
 const fs = require('fs')
 const path = require('path')
 require('dotenv').config()
@@ -7,42 +7,43 @@ const reportWordsPath = path.resolve(__dirname, './data/report-words.json')
 
 /**
  * 
- * @param {String} word 
- * @param {String} reason 
- * @param {String} author 
- * @param {String} svname 
- * @param {String} svid
- * @param {String} svicon
+ * @param {Object} wordData 
+ * @param {Number} status
  * @returns {EmbedBuilder}
  */
-const reportEmbed = (word, reason, author, svname, svid, svicon) => {
+const reportEmbed = (wordData, status = 0) => {
     return new EmbedBuilder()
         .setColor(13250094)
-        .setThumbnail(svicon)
+        .setThumbnail(wordData.guildIcon)
         .addFields(
             {
                 name: ':regional_indicator_p: Từ báo cáo',
-                value: `**${word}**`,
+                value: `**${wordData.word}**`,
                 inline: true
             },
             {
                 name: ':bulb: Lý do',
-                value: reason,
+                value: wordData.reason,
                 inline: true
             },
             {
                 name: ':bust_in_silhouette: Người gửi',
-                value: author,
+                value: wordData.user,
                 inline: true
             },
             {
                 name: ':shield: Máy chủ',
-                value: svname,
+                value: wordData.guildName,
                 inline: true
             },
             {
                 name: ':id: ID server',
-                value: svid,
+                value: wordData.guildId,
+                inline: true
+            },
+            {
+                name: 'Trạng thái',
+                value: (status === 0) ? ':clock12: Đang chờ' : (status === 1) ? ':white_check_mark: Đã đồng ý' : ':x: Đã từ chối',
                 inline: true
             }
         )
@@ -78,14 +79,66 @@ module.exports = {
             let word = interaction.options.getString('word')
             let reason = interaction.options.getString('reason') ?? 'No reason provided.'
 
-            client.channels.cache.get(REPORT_CHANNEL).send({
-                embeds: [reportEmbed(word, reason, interaction.user.username, interaction.guild.name, interaction.guildId, interaction.guild.iconURL({ dynamic: true }))]
-            })
-
-            return interaction.reply({
+            await interaction.reply({
                 content: `Đã báo cáo từ **${word}**`,
                 ephemeral: true
             })
+
+            const acceptButton = new ButtonBuilder()
+                .setCustomId('accept')
+                .setLabel('Đồng ý')
+                .setStyle(ButtonStyle.Success)
+
+            const declineButton = new ButtonBuilder()
+                .setCustomId('decline')
+                .setLabel('Từ chối')
+                .setStyle(ButtonStyle.Danger)
+
+            const row = new ActionRowBuilder()
+                .addComponents(acceptButton, declineButton)
+            
+            const wordData = {
+                word,
+                reason,
+                user: interaction.user.username,
+                guildName: interaction.guild.name,
+                guildId: interaction.guildId,
+                guildIcon: interaction.guild.iconURL({ dynamic: true })
+            }
+
+            const msg = await client.channels.cache.get(REPORT_CHANNEL).send({
+                embeds: [reportEmbed(wordData)],
+                components: [row]
+            })
+
+            const filter = i => i.customId === 'accept' || i.customId === 'decline'
+
+            const collection = msg.createMessageComponentCollector({filter})
+
+            collection.on('collect', async i => {
+
+                if (!i.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+                    return i.reply({
+                        content: 'Bạn không có quyền này',
+                        ephemeral: true
+                    })
+                }
+        
+                let status
+        
+                if (i.customId === 'accept') {
+                    status = 1
+                } else {
+                    status = 2
+                }
+
+                await msg.edit({
+                    embeds: [reportEmbed(wordData, status)],
+                    components: []
+                })
+                return
+            })
+
         }
 
     }
