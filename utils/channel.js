8dc1dff17ae04ac7c6d00c79f0979e0d7fa96c44
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-06-17 15:54:44 +0700

    :fire: add channel utils, set channel function

---
+++ utils/channel.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+
+const dataPath = path.resolve(__dirname, '../data/data.json')
+
+/**
+ * 
+ * @param {Number} guildId 
+ * @param {Number} channelId 
+ */
+const setChannel = (guildId, channelId) => {
+    const dataChannel = require(dataPath)
+    dataChannel[guildId] = {
+        channel: channelId
+    }
+
+    fs.writeFileSync(dataPath, JSON.stringify(dataChannel))
+}
+
+module.exports = {}
